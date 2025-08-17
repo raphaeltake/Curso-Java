c9b55ba6b9c91c49f05977faa4c2c9c123527cb2
raphaeltake <raph.takeuchi@gmail.com>
2025-08-17 00:07:29 -0300

    Uptade date

--- conteúdo/Date/ConverteDataGlobal-Local/src/Main.java
+++ conteúdo/Date/ConverteDataGlobal-Local/src/Main.java
@@ -1,5 +1,38 @@
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 public class Main {
     public static void main(String[] args) {
+        LocalDate d04 = LocalDate.parse("2022-07-20");
+        LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+        Instant d06 = Instant.parse("2022-07-20T01:30:26Z"); // Horário de Londres
 
+        //Somente data:
+        LocalDate r1 = LocalDate.ofInstant(d06, ZoneId.systemDefault());
+        LocalDate r2 = LocalDate.ofInstant(d06, ZoneId.of("Portugal"));
+        System.out.println("Brasil = " + r1);
+        System.out.println("Portugal = " + r2);
+
+        System.out.println();
+
+        //Data e Hora:
+        LocalDateTime r3 = LocalDateTime.ofInstant(d06, ZoneId.systemDefault());
+        LocalDateTime r4 = LocalDateTime.ofInstant(d06, ZoneId.of("Portugal"));
+        System.out.println("Brasil = " + r3);
+        System.out.println("Portugal = " + r4);
+
+        System.out.println();
+
+        //Obter valores separados das datas:
+        System.out.println("d04 dia = " + d04.getDayOfMonth());
+        System.out.println("d04 mês = " + d04.getMonth());
+        System.out.println("d04 ano = " + d04.getYear());
+
+        System.out.println();
+
+        System.out.println("d05 hora = " + d05.getHour());
+        System.out.println("d05 hora = " + d05.getMinute());
     }
 }
