c4bbc407e4c9046fa764cc3c916827002a0a908f
raphaeltake <raph.takeuchi@gmail.com>
2025-09-19 17:42:26 -0300

    IntroÃdução polimorfismo

--- conteúdo/Herança/Heranca/src/Main.java
+++ conteúdo/Herança/Heranca/src/Main.java
@@ -34,17 +34,31 @@ public class Main {
 
 
 
-        //Sobreposição
-        Account acc1 = new Account(1001, "Alex", 1000.0);
-        acc1.withdraw(200.0);
-        System.out.println(acc1.getBalance());
+//        //Sobreposição
+//        Account acc1 = new Account(1001, "Alex", 1000.0);
+//        acc1.withdraw(200.0);
+//        System.out.println(acc1.getBalance());
+//
+//        Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
+//        acc2.withdraw(200.0);
+//        System.out.println(acc2.getBalance());
+//
+//        Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
+//        acc3.withdraw(200.0);
+//        System.out.println(acc3.getBalance());
 
-        Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
-        acc2.withdraw(200.0);
-        System.out.println(acc2.getBalance());
 
-        Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
-        acc3.withdraw(200.0);
-        System.out.println(acc3.getBalance());
+
+
+        //Introdução polimorfismo
+        Account x = new Account(1020, "Alex", 1000.0);
+        Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+
+        x.withdraw(50.0);
+        y.withdraw(50.0);
+
+        System.out.println(x.getBalance());
+        System.out.println(y.getBalance());
+
     }
 }
