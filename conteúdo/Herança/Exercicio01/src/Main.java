e166e208b69ca0fcc35c0a5de112a68a2bf5ff51
raphaeltake <raph.takeuchi@gmail.com>
2025-09-19 18:36:09 -0300

    Update Employee and OutsourceEmployee and Main creation

--- conteúdo/Herança/Exercicio01/src/Main.java
+++ conteúdo/Herança/Exercicio01/src/Main.java
@@ -1,5 +1,55 @@
+import entities.Employee;
+import entities.OutsourceEmployee;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter the number of employees: ");
+        int qtde = sc.nextInt();
+        Employee[] vect = new Employee[qtde];
+        inputData(vect, sc);
+        showPayment(vect);
+
+    }
+
+    public static void inputData(Employee[] vect, Scanner sc){
+        for (int i = 0; i < vect.length; i++){
+            sc.nextLine();
+            System.out.printf("%nEmployee #%d data:", i+1);
+            System.out.printf("%nOutsorced (y/n)? ");
+            String outsorced = sc.next().toUpperCase();
+
+            sc.nextLine();
+
+            System.out.print("Name: ");
+            String name = sc.nextLine();
+
+            System.out.print("Hours: ");
+            int hours = sc.nextInt();
+
+            System.out.print("Value per hour: ");
+            double valuePerHour = sc.nextDouble();
+
+            if (outsorced.equals("Y")){
+                System.out.print("Additional charge: ");
+                double additionalCharge = sc.nextDouble();
+                vect[i] = new OutsourceEmployee(name, hours, valuePerHour, additionalCharge);
+            } else {
+                vect[i] = new Employee(name, hours, valuePerHour);
+            }
+        }
+    }
+
+    public static void showPayment(Employee[] vect){
+        System.out.println();
+        System.out.println("Payments:");
+        for (Employee employee : vect){
+            System.out.println(employee.getName() + " - $" + String.format("%.2f", employee.payment()));
+        }
     }
 }
--- conteúdo/Herança/Exercicio01/src/entities/OutsourceEmployee.java
+++ conteúdo/Herança/Exercicio01/src/entities/OutsourceEmployee.java
@@ -3,6 +3,22 @@ package entities;
 public class OutsourceEmployee extends Employee{
     private Double additionalCharge;
 
+    public OutsourceEmployee() {
+    }
+
+    public OutsourceEmployee(String name, Integer hours, Double valuePerHour, Double additionalCharge) {
+        super(name, hours, valuePerHour);
+        this.additionalCharge = additionalCharge;
+    }
+
+    public Double getAdditionalCharge() {
+        return additionalCharge;
+    }
+
+    public void setAdditionalCharge(Double additionalCharge) {
+        this.additionalCharge = additionalCharge;
+    }
+
     @Override
     public Double payment(){
         return super.payment() + additionalCharge * 1.10;
