4bafa8d73c0ac87d9a0edf47e0e49f4b468ce5aa
raphaeltake <raph.takeuchi@gmail.com>
2025-09-20 00:32:21 -0300

    UsedProduct class create

---
+++ conteúdo/Herança/Exercicio02/src/entities/UsedProduct.java
@@ -0,0 +1,29 @@
+package entities;
+
+import java.time.LocalDate;
+
+public class UsedProduct extends Product{
+    private LocalDate manufactureDate;
+
+    public UsedProduct() {
+        super();
+    }
+
+    public UsedProduct(String name, Double price, LocalDate manufactureDate) {
+        super(name, price);
+        this.manufactureDate = manufactureDate;
+    }
+
+    public LocalDate getManufactureDate() {
+        return manufactureDate;
+    }
+
+    public void setManufactureDate(LocalDate manufactureDate) {
+        this.manufactureDate = manufactureDate;
+    }
+
+    @Override
+    public String priceTag() {
+        return String.format("%s (used) $ %.2f (Manufacture date: ", getName(), getPrice()) + manufactureDate;
+    }
+}
