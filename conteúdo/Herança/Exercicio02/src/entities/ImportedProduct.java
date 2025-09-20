459d3226f64b99b822a47226d358df0b9926e347
raphaeltake <raph.takeuchi@gmail.com>
2025-09-20 00:27:50 -0300

    ImportedProduct create

---
+++ conteúdo/Herança/Exercicio02/src/entities/ImportedProduct.java
@@ -0,0 +1,31 @@
+package entities;
+
+public class ImportedProduct extends Product{
+    private Double customFee;
+
+    public ImportedProduct() {
+        super();
+    }
+
+    public ImportedProduct(String name, Double price, Double customFee) {
+        super(name, price);
+        this.customFee = customFee;
+    }
+
+    public Double getCustomFee() {
+        return customFee;
+    }
+
+    public void setCustomFee(Double customFee) {
+        this.customFee = customFee;
+    }
+
+    public Double totalPrice(){
+        return getPrice() + customFee;
+    }
+
+    @Override
+    public String priceTag() {
+        return super.priceTag() + String.format(" (Custom fee: $ %.2f)", customFee);
+    }
+}
